475bc54c5daf49e4fcaae44f24adfda99f18c96d
Flavien Beninca <flavien@obat.fr>
2024-08-20 11:35:38 +0200

    ghpages

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://www.56kode.com", // replace this with your deployed domain
   author: "Flavien Beninca",
   profile: "https://satnaing.dev/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
