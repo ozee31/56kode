5de775efebdcc6b489fbf1b72f2692f6da8c172f
Flavien Beninca <flavien@obat.fr>
2024-08-22 15:53:42 +0200

    generate tag page for post & techwatch

--- src/utils/getUniqueTags.ts
+++ src/utils/getUniqueTags.ts
@@ -1,6 +1,7 @@
 import { slugifyStr } from "./slugify";
 import type { CollectionEntry } from "astro:content";
 import postFilter from "./postFilter";
+import uniqBy from "lodash.uniqby";
 
 interface Tag {
   tag: string;
@@ -18,6 +19,19 @@ const getUniqueTags = (posts: CollectionEntry<"blog" | "techwatch">[]) => {
     )
     .sort((tagA, tagB) => tagA.tag.localeCompare(tagB.tag));
   return tags;
+};
+
+export const getUniqueTagsForAllPosts = (
+  blogPosts: CollectionEntry<"blog">[],
+  techwatchPosts: CollectionEntry<"techwatch">[]
+): Tag[] => {
+  const postsTags = getUniqueTags(blogPosts);
+  const techwatchTags = getUniqueTags(techwatchPosts);
+  return mergeUniqueTags([...postsTags, ...techwatchTags]);
+};
+
+export const mergeUniqueTags = (tags: Tag[]): Tag[] => {
+  return uniqBy(tags, "tag") as Tag[];
 };
 
 export default getUniqueTags;
